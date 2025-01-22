3a2145c14ab63a1da9dab41e4d8a50559566cc24
Friday79 <fuwuigbe@gmail.com>
2025-01-22 11:37:02 +0000

    add category and votes to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Category
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on', 'upvotes', 'downvotes', 'total_votes')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+    actions = ['upvote_posts', 'downvote_posts']
+
+    def upvote_posts(self, request, queryset):
+        """Admin action to upvote selected posts."""
+        for post in queryset:
+            post.upvotes += 1
+            post.save()
+        self.message_user(request, "Selected posts have been upvoted.")
+
+    upvote_posts.short_description = "Upvote selected posts"
+
+    def downvote_posts(self, request, queryset):
+        """Admin action to downvote selected posts."""
+        for post in queryset:
+            post.downvotes += 1
+            post.save()
+        self.message_user(request, "Selected posts have been downvoted.")
+
+    downvote_posts.short_description = "Downvote selected posts"
 
 
 @admin.register(Comment)
@@ -22,6 +41,14 @@ class CommentAdmin(admin.ModelAdmin):
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)  
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug")  # Display category name and slug in the admin list view
+    prepopulated_fields = {"slug": ("name",)}  # Automatically fill the slug field based on the name
+    search_fields = ("name",)
+    ordering = ("name",)
 
 
 
