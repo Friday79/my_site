9d719a7201ed71943264d2c44680a4d498af49fa
Friday79 <fuwuigbe@gmail.com>
2025-01-25 23:06:45 +0000

    host name updated in settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['restaurant-bliss.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [https://restaurant-bliss-d6d838618de9.herokuapp.com/]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
