44857d891f7b65cdac7b538e8fd2b48b2f67670e
Friday79 <fuwuigbe@gmail.com>
2025-01-26 12:58:58 +0000

    change in the letter t to capital letter in base .html

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -29,9 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['restaurant-bliss-d6d838618de9.herokuapp.com']
+ALLOWED_HOSTS = ['restaurant-bliss-d6d838618de9.herokuapp.com', 'localhost', '127.0.0.1' ]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
