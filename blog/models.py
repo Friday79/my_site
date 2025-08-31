ace5ac87186dc112642b884297d252d40d40f2a8
Friday79 <fuwuigbe@gmail.com>
2025-08-31 11:21:28 +0000

    subscribe models added in blog models

--- blog/models.py
+++ blog/models.py
@@ -54,8 +54,9 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,
-                             related_name="comments")
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE,related_name="comments"
+    )
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
@@ -66,5 +67,12 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"     
+        return f"Comment {self.body} by {self.name}"  
+       
 
+class Subscriber(models.Model):
+    email = models.EmailField(unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email    
