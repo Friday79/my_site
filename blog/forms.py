4ee2ff93cbfc9cc603e5aff4e1b5ff00b74b80c9
Friday79 <fuwuigbe@gmail.com>
2025-08-31 12:31:13 +0000

    base index and category_posts.html added

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,31 @@
-from .models import Comment
+from .models import Comment, Post, Category, Subscriber
 from django import forms
+
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'category', 'featured_image', 'excerpt', 'content', 'status']  
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ["name"]
+        labels = {'name': 'Category name'}
+
+
+class SubscriberForm(forms.ModelForm):
+    class Meta:
+        model = Subscriber
+        fields = ['email']
+        
+
+    
