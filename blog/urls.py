55e3eb0c0afe2f40d6dec6ed9ff5056031b13a65
Friday79 <fuwuigbe@gmail.com>
2025-08-31 11:49:06 +0000

    subscribe , post create and crud functionality urls created in blog urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,16 @@
 from . import views
 from django.urls import path
+from .views import SubscribeView
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('upvote/<slug:slug>', views.PostUpvote.as_view(), name='post_upvote'),
     path('downvote/<slug:slug>', views.PostDownvote.as_view(), name='post_downvote'),
-    path("category/<slug:slug>/", views.CategoryPostList.as_view(), name="category_posts"),
+    path('post/update/<slug:slug>', views.PostUpdateView.as_view(), name='post_update'),
+    path('<slug:slug>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('subscribe/', SubscribeView.as_view(), name='subscribe'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('category/<slug:slug>/', views.CategoryPostList.as_view(), name="category_posts"),
 ]
