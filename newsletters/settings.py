7ee4d16b37408a547693fdbd29a2ccf2efc87d49
Friday79 <fuwuigbe@gmail.com>
2025-09-10 10:08:02 +0000

    satic files configure for deployment

--- newsletters/settings.py
+++ newsletters/settings.py
@@ -193,7 +193,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 if DEBUG:
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 else:
