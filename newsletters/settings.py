85d5b27fb5a378684209d33444e06e741e9a60ba
Friday79 <fuwuigbe@gmail.com>
2025-09-15 19:43:28 +0000

    deployment

--- newsletters/settings.py
+++ newsletters/settings.py
@@ -201,7 +201,7 @@ cloudinary.config(
 )
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
